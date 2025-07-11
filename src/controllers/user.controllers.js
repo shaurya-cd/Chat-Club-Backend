2b2986ba0d87ac007e58e0eafcf07bc6e3817728
shaurya <2447shaurya48@gmail.com>
2025-07-11 09:37:34 +0530

    test backend

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -16,7 +16,7 @@ const createToken = (id, res) =>{
     res.cookie("token",token,{
         maxAge: 7 * 24 * 60 * 60 * 1000,
         httpOnly:true,
-        sameSite: "strict",
+        sameSite: "None",
         secure:process.env.NODE_ENV !== "development"  
     })
 
